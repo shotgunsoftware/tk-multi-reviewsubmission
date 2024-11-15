f35ed479855a70a7287e3a3347ada0beb613f668
Julien Langlois <julien.langlois@autodesk.com>
2024-11-15 11:14:32 -0800

    Remove six requirements

--- hooks/tk-nuke/render_media.py
+++ hooks/tk-nuke/render_media.py
@@ -13,8 +13,6 @@ import os
 import sys
 import nuke
 
-from tank_vendor import six
-
 HookBaseClass = sgtk.get_hook_baseclass()
 
 
@@ -202,7 +200,7 @@ class RenderMedia(HookBaseClass):
 
         # apply any additional knob settings provided by the hook. Now that the knob has been
         # created, we can be sure specific file_type settings will be valid.
-        for knob_name, knob_value in six.iteritems(wn_settings):
+        for knob_name, knob_value in wn_settings.items():
             if knob_name != "file_type":
                 node.knob(knob_name).setValue(knob_value)
 
